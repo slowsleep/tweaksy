a5b97969b47125d5cfdb5b13c2167a5d53f72356
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-04-12 09:31:51 +0300

    refactor: update error message in useTheme

--- src/providers/ThemeProvider/useTheme.ts
+++ src/providers/ThemeProvider/useTheme.ts
@@ -4,7 +4,7 @@ import { ThemeContext, ThemeContextProps } from "./ThemeContext";
 export const useTheme = (): ThemeContextProps => {
     const context = useContext(ThemeContext);
     if (!context) {
-        throw new Error("useTheme must be used within a TweaksyProvider");
+        throw new Error("useTheme must be used within a ThemeProvider");
     }
     return context;
 };
