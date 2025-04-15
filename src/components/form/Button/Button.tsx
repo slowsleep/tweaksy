856127d5ce221d9b651fd14e707f38586f4dcbe3
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-04-15 20:22:22 +0300

    feat: add Button form component with own styles

---
+++ src/components/form/Button/Button.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Button.css";
+import { useTheme } from "@/providers/ThemeProvider/useTheme";
+import { Color } from "@/types/index";
+
+type ButtonProps = {
+    children: React.ReactNode;
+    onClick?: () => void;
+    className?: string;
+    disabled?: boolean;
+    size?: "sm" | "md" | "lg";
+    customColor?: Color;
+    variant?: "solid" | "outline";
+    shadow?: "none" | "sm" | "md" | "lg";
+    [key: string]: any;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+    children,
+    onClick,
+    className = "",
+    disabled = false,
+    size = "md",
+    customColor,
+    variant = "solid",
+    shadow = "none",
+    ...props
+}) => {
+    const { theme, color, radius } = useTheme();
+
+    let classes =  `
+        tw-button
+        ${size}
+        ${variant}
+        tw-radius-${radius}
+        ${shadow !== "none" ? "tw-shadow-" + shadow : ""}
+        tw-${theme}
+        tw-color-${customColor || color}
+        ${className}
+    `.trim();
+
+    return (
+        <button
+            className={classes}
+            onClick={onClick}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
