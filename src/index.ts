7e623204d00d772d60fc27c25104eb497a89c075
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-04-15 23:00:38 +0300

    feat: extend root index file with additional exports and styles

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from './components'
+import './styles/colors.css';
+import './styles/borders.css';
+import './styles/shadows.css';
+import './styles/layout.css';
+
+export * from './components';
+export { ThemeProvider } from './providers/ThemeProvider/ThemeProvider';
+export * from './theme/index';
+export * from './types';
